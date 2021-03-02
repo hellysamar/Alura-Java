38e607d36d95a2cd0fe145bb9a21a2c139f99149
Hellysamar <hellysamar@hotmail.com>
2021-03-01 23:05:29 -0300

    Uso do tipo int (inteiro).

--- sintaxe-basica/src/sintaxe/Programa.java
+++ sintaxe-basica/src/sintaxe/Programa.java
@@ -1,9 +1,10 @@
+package sintaxe;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo!!");
+		System.out.println("Olá TESTE!!");
 		
 	}
 }
---
+++ sintaxe-basica/src/sintaxe/sintaxeVariaveisEFluxo.java
@@ -0,0 +1,23 @@
+package sintaxe;
+
+public class sintaxeVariaveisEFluxo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá novo Teste!!!");
+		
+		int idade;
+		idade = 27;
+		
+		int anos = 2;
+		
+		int soma = idade + anos;
+		
+		String nome;
+		nome = "Hellysamar";
+		
+		
+		System.out.println(nome + " tem " + idade + " anos e daqui " 
+							+ anos + " anos já estará com " + soma + ".");
+	}
+}
